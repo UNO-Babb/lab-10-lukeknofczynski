96d945510b3604b989bad321008c13817dde47e0
lukeknofczynski <lknofczynski@unomaha.edu>
2025-04-16 15:06:10 +0000

    all finished

--- main.py
+++ main.py
@@ -2,3 +2,20 @@
 #Name:
 #Date:
 #Assignment:
+
+import airlines
+airlines = airlines.get_airports()
+
+num_items = len(airlines)
+years = []
+canceled = []
+
+for spot in range(num_items):
+    year = airlines[spot]["Time"]["Year"]
+    canceledFlights = airlines[spot]["Statistics"]["Flights"]["Cancelled"]
+    years.append(year)
+    canceled.append(canceledFlights)
+
+import matplotlib.pyplot as plt
+plt.plot(years, canceled, "ro")
+plt.savefig("output")
